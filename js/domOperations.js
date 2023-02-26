6d0c84a270967dd042ff9f32a709b7558884eb7a
yehor <ivashin.egor98@gmail.com>
2023-02-26 20:06:49 +0200

    deploy 0.1

---
+++ js/domOperations.js
@@ -0,0 +1,57 @@
+function showResultInDomKvadrat() {
+  const dlinaStoroniKvadrata = document.querySelector(
+    '#dlinaStoroniKvadrata'
+  ).value;
+
+  const ploschadKvadrata = document.querySelector('#ploschadKvadrata');
+
+  ploschadKvadrata.innerHTML = getSquareArea(dlinaStoroniKvadrata);
+}
+
+function showResultInDomPriamougolnika() {
+  const pervayaStoronaPriamougolnika = document.querySelector(
+    '#pervayaStoronaPriamougolnika'
+  ).value;
+
+  const drugayaStoronaPriamougolnika = document.querySelector(
+    '#drugayaStoronaPriamougolnika'
+  ).value;
+
+  const ploschadPriamougolnika = document.querySelector(
+    '#ploschadPriamougolnika'
+  );
+
+  ploschadPriamougolnika.innerHTML = getSquareRectangle(
+    pervayaStoronaPriamougolnika,
+    drugayaStoronaPriamougolnika
+  );
+}
+
+function showResultInDomParalelograma() {
+  const dlinaStoronyParalelograma = document.querySelector(
+    '#dlinaStoronyParalelograma'
+  ).value;
+
+  const dlinaVisotyParalelograma = document.querySelector(
+    '#dlinaStoronyParalelograma'
+  ).value;
+
+  const ploschadParalelograma = document.querySelector(
+    '#ploschadParalelograma'
+  );
+
+  ploschadParalelograma.innerHTML = getSquareParalelograma(
+    dlinaStoronyParalelograma,
+    dlinaVisotyParalelograma
+  );
+}
+
+function showResultInDomRomba() {
+  const dlinaStoronyRomba = document.querySelector('#dlinaStoronyRomba').value;
+
+  const dlinaVisotyRomba = document.querySelector('#dlinaVisotyRomba').value;
+
+  const ploschadRomba = document.querySelector('#ploschadRomba');
+
+  ploschadRomba.innerHTML = getSquareRomb(dlinaStoronyRomba, dlinaVisotyRomba);
+}
---
+++ js/libraryFormula.js
@@ -0,0 +1,15 @@
+function getSquareArea(sideLength) {
+  return sideLength * sideLength;
+}
+
+function getSquareRectangle(long, weight) {
+  return long * weight;
+}
+
+function getSquareParalelograma(long, hight) {
+  return long * hight;
+}
+
+function getSquareRomb(long, hight) {
+  return long * hight;
+}
